2316ab77bc927dd188744d547f15872363f12926
hemaswathi2002 <hemaswathi2002@gmail.com>
2024-05-02 11:44:31 +0530

    customer
    customer

---
+++ frontend/my-app/src/Components/CustomerDashboard/CustomerDetails.js
@@ -0,0 +1,5 @@
+export default function CustomerDetails(){
+    return(
+        <div></div>
+    )
+}
--- frontend/my-app/src/Components/usersControl/usersControl.js
+++ frontend/my-app/src/Components/usersControl/usersControl.js
@@ -2,7 +2,7 @@ import { jwtDecode } from "jwt-decode"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Owner from "../OwnerDashboard/Owner"
-
+import Customer from '../CustomerDashboard/Customer'
 export default function UsersControl(){
     const navigate=useNavigate()
     const token=localStorage.getItem('token')
