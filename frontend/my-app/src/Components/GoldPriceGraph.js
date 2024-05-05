192186e90017fd43acc85a016b51302318df0101
Samiksha Anil  Wagrekar <samikshawagrekar@gmail.com>
2024-05-05 23:19:22 +0530

    css chart

--- frontend/my-app/src/Components/GoldPriceGraph.js
+++ frontend/my-app/src/Components/GoldPriceGraph.js
@@ -1,48 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import {Chart as Chartjs} from 'chart.js/auto'
-import { Line, TimeScale } from 'react-chartjs-2';
-import "chartjs-adapter-date-fns" 
+
+import { Line } from 'react-chartjs-2'
+import 'chartjs-adapter-date-fns'
+import 'chartjs-plugin-annotation'
 
 const GoldPriceGraph = () => {
-  const [goldPrices, setGoldPrices] = useState(null);
+  const [goldPrices, setGoldPrices] = useState(null)
 
   useEffect(() => {
     const fetchGoldPrices = async () => {
       try {
-        const response = await axios.get('http://localhost:3009/api/goldprice');
-        console.log("Golllddldldl",response.data)
-        if(response.data.length>0){
-          setGoldPrices({
-            labels:response.data.map((individualData)=>individualData.goldPrice),
-            datasets:[{
-              label:'GoldPrice',
-              data:response.data.map((individualData)=>individualData.timestamp),
-            }]
-          });
+        // const response = await axios.get('http://localhost:3009/api/goldprice')
+        // console.log("Gold Prices Data:", response.data)
+        // if (response.data.length > 0) {
+        //   const formattedData = {
+        //     labels: response.data.map((individualData) => formatDate(individualData.timestamp)),
+        //     datasets: [{
+        //       label: 'Gold Price',
+        //       data: response.data.map((individualData) => individualData.goldPrice),
+        //       borderColor: 'lightblue',
+              
 
-        }else{
-          console.log("No data found")
-        }
-     
+        //     }]
+        //   }
+        //   setGoldPrices(formattedData)
+        // } else {
+        //   console.log("No data found")
+        // }
       } catch (error) {
-        console.error('Error fetching gold prices:', error);
+        console.error('Error fetching gold prices:', error)
       }
-    };
+    }
 
-    fetchGoldPrices();
+    fetchGoldPrices()
 
-  }, []);
+    const interval = setInterval(fetchGoldPrices, 3600000)
 
+    fetchGoldPrices()
 
-return(
-  <div className="wrapper">
-  {goldPrices!==null?(
-    <Line data={goldPrices}/>
-  ):(
-    <div>Gold Price is Null</div>
-  )}
-  </div>
-)
+    return () => clearInterval(interval)
+  }, [])
+
+  const formatDate = (timestamp) => {
+    const date = new Date(parseInt(timestamp) * 1000) 
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+  }
+  const options = {
+    scales: {
+      x: {
+        ticks: {
+          color: 'darktblue', 
+        },
+      },
+      y: {
+        ticks: {
+          color: 'darktblue',
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="wrapper">
+      {goldPrices !== null ? (
+        <Line data={goldPrices}options={options} />
+      ) : (
+        <div>Gold Price Data is Null</div>
+      )}
+    </div>
+  )
 }
+
 export default GoldPriceGraph
